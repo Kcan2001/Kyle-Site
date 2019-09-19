e799d7124601d6453c6276bdb7e793deda44e20a
Cannon <kcanno045@cable.comcast.com>
2019-09-19 14:11:36 -0400

    Updated site

--- src/pages/next.js
+++ src/pages/next.js
@@ -10,6 +10,11 @@ const NextPage = () => (
     <SEO title="What's Next Page" />
     <div className="whats-next-page-grid-container">
         <Card 
+            title="{ Developing } Customer Journeys " 
+            description="Talk given on September 19th, 2019 (Slides provided below)"
+            imageLink={"https://secure.meetupstatic.com/photos/event/d/d/1/a/highres_484976602.jpeg"}
+            link="https://docs.google.com/presentation/d/1jLTzjDxUf-vMPLATp_zmASS3oE3cQjQw3FTS9Gca9P8/edit?usp=sharing"/>
+        <Card 
             title="Large Scale Application Organization in React" 
             description="Talk given on March 18th, 2019 (Slides provided below)"
             imageLink={"https://secure.meetupstatic.com/photos/event/8/5/3/4/highres_479494100.jpeg"}
